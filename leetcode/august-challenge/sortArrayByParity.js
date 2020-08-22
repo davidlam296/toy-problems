999a17425a59a5949e4d58c7ae39b881d2bf6c6c
David <david.lam296@gmail.com>
2020-08-22 00:26:18 -0400

    Sort Array By Partiy Complete

---
+++ leetcode/august-challenge/sortArrayByParity.js
@@ -0,0 +1,16 @@
+const sortArrayByParity = (A) => {
+  const isEven = (n) => {
+    return n % 2 === 1;
+  };
+
+  A.sort((a, b) => {
+    const aEven = isEven(a);
+    const bEven = isEven(b);
+
+    if ((aEven && bEven) || (!aEven && !bEven)) return 0;
+    else if (aEven && !bEven) return 1;
+    else return -1;
+  });
+
+  return A;
+};
