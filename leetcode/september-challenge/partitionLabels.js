c0c40de2051535d7f467608805ceb909d3479c7c
David <david.lam296@gmail.com>
2020-09-05 00:51:30 -0400

    Partition Labels Complete

---
+++ leetcode/september-challenge/partitionLabels.js
@@ -0,0 +1,35 @@
+const partitionLabels = (S) => {
+  const last = new Array(26).fill(0);
+
+  for (let i = 0; i < S.length; i++) {
+    const charIndex = S.charCodeAt(i) - 'a'.charCodeAt(0);
+    last[charIndex] = i;
+  }
+
+  const result = [];
+  let startOfPartition = 0;
+  let charsInPartition = new Set();
+
+  for (let i = 0; i < S.length; i++) {
+    const charIndex = S.charCodeAt(i) - 'a'.charCodeAt(0);
+    charsInPartition.add(charIndex);
+
+    if (i === last[charIndex]) {
+      let validPartition = true;
+
+      for (const charIndex of charsInPartition) {
+        if (last[charIndex] > i) {
+          validPartition = false;
+          break;
+        }
+      }
+
+      if (validPartition) {
+        result.push(i - startOfPartition + 1);
+        startOfPartition = i + 1;
+      }
+    }
+  }
+
+  return result;
+};
