e7a9797472e47c58d87720b81b728e4ac93a0c72
David <david.lam296@gmail.com>
2020-12-15 01:12:23 -0500

    Advent of Code - Day 15 Complete

---
+++ advent_of_code_2020/day15/15.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const data = fs
+  .readFileSync(__dirname + '/15.txt', 'utf8')
+  .split('\n')[0]
+  .split(',');
+
+const findNum = (nums, target) => {
+  const END = target;
+  const mem = {};
+
+  nums.forEach((num, idx) => {
+    mem[num] = idx + 1;
+  });
+
+  let prev = 0;
+  if (!mem[prev]) mem[prev] = nums.length + 1;
+
+  for (let turn = nums.length + 2; turn <= END; turn++) {
+    if (mem[prev] === turn - 1) {
+      prev = 0;
+    } else {
+      const curr = turn - 1 - mem[prev];
+
+      mem[prev] = turn - 1;
+      prev = curr;
+
+      if (!mem[curr]) mem[curr] = turn;
+    }
+  }
+
+  return prev;
+};
+
+const part1 = (nums) => {
+  return findNum(nums, 2020);
+};
+
+const part2 = (nums) => {
+  return findNum(nums, 30000000);
+};
+
+console.log(part1(data)); // Answer: 257
+console.log(part2(data)); // Answer: 8546398
