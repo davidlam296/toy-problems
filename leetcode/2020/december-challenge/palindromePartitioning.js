e63b6c57c5a6f5c141eca669c5aa8990ab29bc61
David <david.lam296@gmail.com>
2021-01-08 13:28:20 -0500

    Update Palindrome Partitioning

--- leetcode/2020/december-challenge/palindromePartitioning.js
+++ leetcode/2020/december-challenge/palindromePartitioning.js
@@ -13,10 +13,39 @@ const partition = (s) => {
     if (remaining.length === 0) return result.push(curr);
 
     for (let len = 1; len <= remaining.length; len++) {
-      const subStr = remaining.substr(0, len);
+      const subStr = remaining.slice(0, len);
 
-      if (isPalindrome(subStr))
-        search(remaining.substr(len), [...curr, subStr]);
+      if (isPalindrome(subStr)) search(remaining.slice(len), [...curr, subStr]);
+    }
+  };
+
+  search(s);
+
+  return result;
+};
+
+const partition = (s) => {
+  const result = [];
+
+  const isPalindrome = (str, start = 0, end = str.length - 1) => {
+    while (start <= end) {
+      if (str[start++] !== str[end--]) return false;
+    }
+
+    return true;
+  };
+
+  const search = (remaining, curr = []) => {
+    if (remaining.length === 0) return result.push([...curr]);
+
+    for (let len = 1; len <= remaining.length; len++) {
+      const subStr = remaining.slice(0, len);
+
+      if (isPalindrome(subStr)) {
+        curr.push(subStr);
+        search(remaining.slice(len), curr);
+        curr.pop();
+      }
     }
   };
 
