84596eb390c28e760dc6dae69808d8ed0d261f75
David <david.lam296@gmail.com>
2021-02-02 14:38:06 -0500

    Trim A Binary Search Tree Complete

---
+++ leetcode/2021/february-challenge/trimABinarySearchTree.js
@@ -0,0 +1,13 @@
+const trimBST = (root, low, high) => {
+  if (!root) {
+    return null;
+  } else if (root.val < low) {
+    return trimBST(root.right, low, high);
+  } else if (root.val > high) {
+    return trimBST(root.left, low, high);
+  } else {
+    root.left = trimBST(root.left, low, high);
+    root.right = trimBST(root.right, low, high);
+    return root;
+  }
+};
