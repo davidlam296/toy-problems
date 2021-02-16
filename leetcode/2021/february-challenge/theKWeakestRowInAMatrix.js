09d7c7ed7a8e855b37258fff6c6bbf09a5141d69
David <david.lam296@gmail.com>
2021-02-15 22:05:20 -0500

    Update The K Weakest Row In A Matrix

--- leetcode/2021/february-challenge/theKWeakestRowInAMatrix.js
+++ leetcode/2021/february-challenge/theKWeakestRowInAMatrix.js
@@ -13,3 +13,12 @@ const kWeakestRows = (mat, k) => {
 
   return result;
 };
+
+const kWeakestRows = (mat, k) =>
+  mat
+    .map((row, index) => {
+      return [row.filter((n) => n === 1).length, index];
+    })
+    .sort(([s1, i1], [s2, i2]) => (s1 === s2 ? i1 - i2 : s1 - s2))
+    .slice(0, k)
+    .map((row) => row[1]);
